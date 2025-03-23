71fbb9e4d4e91326a3e05c3244d487f82518bd16
targerian <moh.ali.1593@gmail.com>
2025-03-23 23:06:52 +0200

    added a little discription in the navbar

--- src/screens/layout/navbar.tsx
+++ src/screens/layout/navbar.tsx
@@ -3,7 +3,10 @@ import Image from "next/image";
 export function Navbar() {
   return (
     <div className="w-full h-16 bg-gradient-to-r from-slate-900 to-slate-700 z-50 pt-5 pb-3 ">
-      <Image width={100} height={50} src='/images/logo.png' alt='logo' priority />
+      <div className="flex justify-between items-center w-full">
+        <Image width={100} height={50} src='/images/logo.png' alt='logo' priority />
+        <span className="text-xs text-slate-300">Your lovely bill splitter!</span>
+      </div>
     </div>
   )
 }
